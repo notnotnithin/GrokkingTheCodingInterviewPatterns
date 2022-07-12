b37d58819887ec397daadf80dc0a439012569d9c
Nithin Prasad <nithin2889@gmail.com>
2022-07-12 13:34:12 +0530

    Smallest subarray with greater sum (brute force and efficient solutions)

--- SlidingWindow/src/main/java/com/dawn/grokking/patterns/SmallestSubarrayWithGreaterSum003.java
+++ SlidingWindow/src/main/java/com/dawn/grokking/patterns/SmallestSubarrayWithGreaterSum003.java
@@ -2,4 +2,25 @@ package com.dawn.grokking.patterns;
 
 public class SmallestSubarrayWithGreaterSum003 {
 
+  public static void main(String[] args) {
+    System.out.println(
+        "Smallest subarray length is: "
+            + SmallestSubarrayWithGreaterSum003.findMinSubArray(16, new int[] {2, 1, 5, 2, 3, 2}));
+  }
+
+  private static int findMinSubArray(int sum, int[] arr) {
+    int windowStart = 0, windowSum = 0;
+    int minLength = Integer.MAX_VALUE;
+
+    for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+      windowSum += arr[windowEnd];
+
+      while (windowSum >= sum) {
+        minLength = Math.min(minLength, windowEnd - windowStart + 1);
+        windowSum -= arr[windowStart];
+        windowStart++;
+      }
+    }
+    return minLength == Integer.MAX_VALUE ? 0 : minLength;
+  }
 }
