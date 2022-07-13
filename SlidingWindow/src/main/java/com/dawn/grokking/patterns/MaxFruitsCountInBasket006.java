dfb792e21ff9da67180de795db7ae8b1beb53fdd
Nithin <nithin2889@gmail.com>
2022-07-13 18:07:18 +0530

    Max fruits count of 2 types in a basket (efficient solution)

--- SlidingWindow/src/main/java/com/dawn/grokking/patterns/MaxFruitsCountInBasket006.java
+++ SlidingWindow/src/main/java/com/dawn/grokking/patterns/MaxFruitsCountInBasket006.java
@@ -6,23 +6,22 @@ import java.util.Map;
 public class MaxFruitsCountInBasket006 {
 
   public static void main(String[] args) {
-    char[] treeArr = new char[] {'A', 'B', 'C', 'B', 'B', 'C'};
-    int k = 2;
+    char[] treeArr = new char[] {'A', 'B', 'C', 'A', 'C'};
     System.out.println(
-        "Maximum number of fruits are: " + MaxFruitsCountInBasket006.maxFruitsInBasket(treeArr, k));
+        "Maximum number of fruits are: " + MaxFruitsCountInBasket006.maxFruitsInBasket(treeArr));
   }
 
-  public static int maxFruitsInBasket(char[] treeArr, int k) {
+  public static int maxFruitsInBasket(char[] treeArr) {
     int windowStart = 0, maxLength = Integer.MIN_VALUE;
     Map<Character, Integer> frequencyMap = new HashMap<>();
 
     for (int windowEnd = 0; windowEnd < treeArr.length; windowEnd++) {
       frequencyMap.put(treeArr[windowEnd], frequencyMap.getOrDefault(treeArr[windowEnd], 0) + 1);
 
-      if (frequencyMap.size() == k) {
+      if (frequencyMap.size() == 2) {
         maxLength = Math.max(maxLength, (windowEnd - windowStart + 1));
       } else {
-        while (frequencyMap.size() > k) {
+        while (frequencyMap.size() > 2) {
           char leftChar = treeArr[windowStart];
           frequencyMap.put(leftChar, frequencyMap.get(leftChar) - 1);
 
