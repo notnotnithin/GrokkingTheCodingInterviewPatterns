fccb23ba4131e838e7839db47c59609e641e926c
Nithin Prasad <nithin2889@gmail.com>
2022-07-18 18:16:21 +0530

    count of anagrams in a given string (efficient solution)

--- SlidingWindow/src/main/java/com/dawn/grokking/patterns/CountOfAnagrams010.java
+++ SlidingWindow/src/main/java/com/dawn/grokking/patterns/CountOfAnagrams010.java
@@ -16,46 +16,42 @@ public class CountOfAnagrams010 {
 
   public static Map<Character, Integer> transformStringToMap(String pattern) {
     Map<Character, Integer> patternStrMap = new HashMap<>();
-    for (int i = 0; i < pattern.length(); i++) {
-      patternStrMap.put(pattern.charAt(i), patternStrMap.getOrDefault(pattern.charAt(i), 0) + 1);
+    for (char ch : pattern.toCharArray()) {
+      patternStrMap.put(ch, patternStrMap.getOrDefault(ch, 0) + 1);
     }
     return patternStrMap;
   }
 
   public static int findOccurrences(String str, String pattern, int k) {
     Map<Character, Integer> patternStrMap;
-    int windowStart = 0, windowEnd = 0;
-    int count, anagramsCount = 0;
+    int windowStart = 0, count, anagramsCount = 0;
 
     patternStrMap = transformStringToMap(pattern);
     count = patternStrMap.size();
 
-    while (windowEnd < str.length()) {
+    for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
       char rightChar = str.charAt(windowEnd);
 
       if (patternStrMap.containsKey(rightChar)) {
-        patternStrMap.put(rightChar, patternStrMap.get(rightChar) - 1);
-        if (patternStrMap.get(rightChar) == 0) {
-          count--;
-        }
+        patternStrMap.put(rightChar, patternStrMap.getOrDefault(rightChar, 0) - 1);
+        if (patternStrMap.get(rightChar) == 0) count++;
+      }
+
+      if (count == patternStrMap.size()) {
+        anagramsCount += 1;
+        windowEnd++;
       }
 
       if ((windowEnd - windowStart + 1) < k) {
         windowEnd++;
-      } else if ((windowEnd - windowStart + 1) == k) {
-        if (count == 0) {
-          anagramsCount = anagramsCount + 1;
-        } else {
-          char leftChar = str.charAt(windowStart);
-          if (patternStrMap.containsKey(leftChar)) {
-            patternStrMap.put(leftChar, patternStrMap.get(leftChar) + 1);
-            if (patternStrMap.get(leftChar) == 1) {
-              count++;
-            }
-            windowStart++;
+      } else {
+        char leftChar = str.charAt(windowStart++);
+        if (patternStrMap.containsKey(leftChar)) {
+          if (patternStrMap.get(leftChar) == 0) {
+            count--;
           }
+          patternStrMap.put(leftChar, patternStrMap.get(leftChar) + 1);
         }
-        windowEnd++;
       }
     }
     return anagramsCount;
