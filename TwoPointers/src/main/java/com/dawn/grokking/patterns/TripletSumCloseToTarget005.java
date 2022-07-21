ef14ba65a51d01c6f309451a85e486de02378efb
Nithin Prasad <nithin2889@gmail.com>
2022-07-21 13:33:39 +0530

    triplet sum closest to the target sum (efficient solution)

--- TwoPointers/src/main/java/com/dawn/grokking/patterns/TripletSumCloseToTarget005.java
+++ TwoPointers/src/main/java/com/dawn/grokking/patterns/TripletSumCloseToTarget005.java
@@ -1,5 +1,37 @@
 package com.dawn.grokking.patterns;
 
+import java.util.Arrays;
+
 public class TripletSumCloseToTarget005 {
 
+  public static void main(String[] args) {
+    int[] arr = {-2, 0, 1, 2};
+    int targetSum = 3;
+    System.out.println(
+        "Triplets whose sum equal to zero using brute force approach are: "
+            + TripletSumCloseToTarget005.searchTripletsCloseToTarget(arr, targetSum));
+  }
+
+  public static int searchTripletsCloseToTarget(int[] arr, int targetSum) {
+    Arrays.sort(arr);
+    int closestSumToTarget = arr[0] + arr[1] + arr[arr.length - 1];
+
+    for (int i = 0; i < arr.length - 2; i++) {
+      int left = i + 1;
+      int right = arr.length - 1;
+
+      while (left < right) {
+        int currentSum = arr[i] + arr[left] + arr[right];
+        if (currentSum > targetSum) {
+          right--;
+        } else {
+          left++;
+        }
+        if (Math.abs(currentSum - targetSum) < Math.abs(closestSumToTarget - targetSum)) {
+          closestSumToTarget = currentSum;
+        }
+      }
+    }
+    return closestSumToTarget;
+  }
 }
