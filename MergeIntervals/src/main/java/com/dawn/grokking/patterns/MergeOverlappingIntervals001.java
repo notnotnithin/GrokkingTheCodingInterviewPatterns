00527bdb417d740812625f87f4006fe1c878962f
Nithin Prasad <nithin2889@gmail.com>
2022-07-29 13:11:25 +0530

    Merge intervals to produce mutually exclusive intervals and to check if intervals are overlapping

--- MergeIntervals/src/main/java/com/dawn/grokking/patterns/MergeOverlappingIntervals001.java
+++ MergeIntervals/src/main/java/com/dawn/grokking/patterns/MergeOverlappingIntervals001.java
@@ -9,7 +9,7 @@ public class MergeOverlappingIntervals001 {
 
   public static void main(String[] args) {
     List<Interval> input = new ArrayList<>();
-    input.add(new Interval(1, 5));
+    input.add(new Interval(1, 2));
     input.add(new Interval(4, 8));
     input.add(new Interval(7, 9));
 
@@ -17,6 +17,9 @@ public class MergeOverlappingIntervals001 {
     for (Interval interval : MergeOverlappingIntervals001.merge(input))
       System.out.print("[" + interval.start + "," + interval.end + "] ");
     System.out.println();
+
+    System.out.println(
+        "Has overlapping intervals? " + MergeOverlappingIntervals001.isIntervalOverlapping(input));
   }
 
   public static List<Interval> merge(List<Interval> intervals) {
@@ -42,6 +45,24 @@ public class MergeOverlappingIntervals001 {
     }
     mergedIntervals.add(new Interval(start, end));
     return mergedIntervals;
+    // time complexity: O(N * logN) + O(N) = O(N * logN)
+  }
+
+  public static boolean isIntervalOverlapping(List<Interval> intervals) {
+    if (intervals.size() < 2) return false;
+
+    Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+    Iterator<Interval> iteratorPtr = intervals.iterator();
+    Interval interval = iteratorPtr.next();
+    int end = interval.end;
+
+    while (iteratorPtr.hasNext()) {
+      interval = iteratorPtr.next();
+      if (interval.start <= end) {
+        return true;
+      }
+    }
+    return false;
     // time complexity: O(N * logN) + O(N) = O(N * logN)
   }
 }
