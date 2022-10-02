0b58787a51d098b3b3ecc1cd741d7a0574d73bb7
Nithin Prasad <nithin2889@gmail.com>
2022-10-03 01:40:37 +0530

    Length of the longest substring with distinct characters

--- SlidingWindow/src/main/java/com/dawn/grokking/patterns/LongestSubstringWithDistinctCharacters007.java
+++ SlidingWindow/src/main/java/com/dawn/grokking/patterns/LongestSubstringWithDistinctCharacters007.java
@@ -6,35 +6,25 @@ import java.util.Map;
 public class LongestSubstringWithDistinctCharacters007 {
 
   public static void main(String[] args) {
-    String s = "abccde";
+    String s = "aabccbbdef";
+
     System.out.println(
         "Length of the longest substring with distinct characters is: "
-            + LongestSubstringWithDistinctCharacters007.findLongestSubstring(s));
+            + LongestSubstringWithDistinctCharacters007.findLongestSubstringEfficient(s));
   }
 
-  public static int findLongestSubstring(String s) {
-    int windowStart = 0, windowEnd = 0, maxLength = Integer.MIN_VALUE;
-    Map<Character, Integer> frequencyMap = new HashMap<>();
-
-    while (windowEnd < s.length()) {
+  public static int findLongestSubstringEfficient(String s) {
+    int windowStart = 0, windowEnd = 0, maxLen = 0;
+    Map<Character, Integer> fMap = new HashMap<>();
+    for (windowEnd = 0; windowEnd < s.length(); windowEnd++) {
       char rightChar = s.charAt(windowEnd);
-      frequencyMap.put(rightChar, frequencyMap.getOrDefault(rightChar, 0) + 1);
+      fMap.put(rightChar, fMap.getOrDefault(rightChar, 0) + 1);
 
-      if (frequencyMap.size() == windowEnd - windowStart + 1) {
-        maxLength = Math.max(maxLength, (windowEnd - windowStart + 1));
-        windowEnd++;
-      } else {
-        while (frequencyMap.size() < windowEnd - windowStart + 1) {
-          char leftChar = s.charAt(windowStart);
-          frequencyMap.put(leftChar, frequencyMap.get(leftChar) - 1);
-
-          if (frequencyMap.get(leftChar) == 0) {
-            frequencyMap.remove(leftChar);
-          }
-          windowStart++;
-        }
+      if (fMap.get(s.charAt(windowEnd)) > 1) {
+        windowStart = windowEnd;
       }
+      maxLen = Math.max(maxLen, windowEnd - windowStart + 1);
     }
-    return maxLength;
+    return maxLen;
   }
 }
