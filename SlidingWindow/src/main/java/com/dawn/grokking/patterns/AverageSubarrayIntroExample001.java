56f1d16b0517beb03c57a18c6fdc6955adbe19f5
Nithin Prasad <nithin2889@gmail.com>
2022-11-20 01:22:29 +0530

    average subarray sum - AlgoLabs implementation

--- SlidingWindow/src/main/java/com/dawn/grokking/patterns/AverageSubarrayIntroExample001.java
+++ SlidingWindow/src/main/java/com/dawn/grokking/patterns/AverageSubarrayIntroExample001.java
@@ -8,13 +8,22 @@ public class AverageSubarrayIntroExample001 {
     int[] arr = new int[] {1, 3, 2, 6, -1, 4, 1, 8, 2};
     int k = 5;
     double[] res = AverageSubarrayIntroExample001.averageSubarrayBruteForce(arr, k);
-    System.out.println("Averages of subarrays of size K using brute force: " + Arrays.toString(res));
+    System.out.println(
+        "Averages of subarrays of size K using brute force: " + Arrays.toString(res));
 
     res = AverageSubarrayIntroExample001.averageSubarrayEfficientSolution(arr, k);
-    System.out.println("Averages of subarrays of size K using efficient solution: " + Arrays.toString(res));
+    System.out.println(
+        "Averages of subarrays of size K using efficient solution: " + Arrays.toString(res));
 
     double result = AverageSubarrayIntroExample001.findMaxAverage(arr, 2);
     System.out.println("Max average of subarrays of size K is : " + result);
+
+    double[] avgArr = AverageSubarrayIntroExample001.findMaxAverageAlgoLabs(arr, k);
+    System.out.println();
+    System.out.println(":::Sliding window solution:::");
+    for (double el : avgArr) {
+      System.out.print(el + "\t");
+    }
   }
 
   // brute force
@@ -53,14 +62,30 @@ public class AverageSubarrayIntroExample001 {
     double windowSum = 0;
     double maxAvgSum = Double.NEGATIVE_INFINITY;
 
-    for(int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+    for (int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
       windowSum += nums[windowEnd];
 
-      if(windowEnd >= k - 1) {
+      if (windowEnd >= k - 1) {
         maxAvgSum = Math.max(windowSum, maxAvgSum);
         windowSum -= nums[windowEnd - (k - 1)];
       }
     }
     return maxAvgSum / k;
+  }
+
+  public static double[] findMaxAverageAlgoLabs(int[] nums, int k) {
+    int n = nums.length;
+    double[] avg = new double[n - k + 1];
+    double winSum = 0.0;
+
+    for (int i = 0; i < k; i++) {
+      winSum += nums[i];
+    }
+    avg[0] = winSum / k;
+    for (int i = 0; i < (n - k); i++) {
+      winSum = winSum - nums[i] + nums[i + k];
+      avg[i + 1] = winSum / k;
+    }
+    return avg;
   }
 }
